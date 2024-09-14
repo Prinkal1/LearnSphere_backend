5ecc87c1bcd435019cddf6181cea8a71d7c3a5dc
Prinkal1 <mahajanprinkal@gmail.com>
2024-09-15 01:42:20 +0530

    controllers and routes done

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,10 +2,8 @@
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 const User = require("../models/User");
-// Configuring dotenv to load environment variables from .env file
 dotenv.config();
 
-// This function is used as middleware to authenticate user requests
 exports.auth = async (req, res, next) => {
 	try {
 		// Extracting JWT from request cookies, body or header
@@ -14,28 +12,23 @@ exports.auth = async (req, res, next) => {
 			req.body.token ||
 			req.header("Authorization").replace("Bearer ", "");
 
-		// If JWT is missing, return 401 Unauthorized response
 		if (!token) {
 			return res.status(401).json({ success: false, message: `Token Missing` });
 		}
 
 		try {
-			// Verifying the JWT using the secret key stored in environment variables
 			const decode = await jwt.verify(token, process.env.JWT_SECRET);
 			console.log(decode);
-			// Storing the decoded JWT payload in the request object for further use
 			req.user = decode;
 		} catch (error) {
-			// If JWT verification fails, return 401 Unauthorized response
 			return res
 				.status(401)
 				.json({ success: false, message: "token is invalid" });
 		}
 
-		// If JWT is valid, move on to the next middleware or request handler
+		// If JWT is valid, move on to the next middleware 
 		next();
 	} catch (error) {
-		// If there is an error during the authentication process, return 401 Unauthorized response
 		return res.status(401).json({
 			success: false,
 			message: `Something Went Wrong While Validating the Token`,
